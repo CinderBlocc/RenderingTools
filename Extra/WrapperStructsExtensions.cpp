9a247456a8e2c0309c8991728ba2e22bf5f0777a
CinderBlocc <cinder.block.rl@gmail.com>
2020-08-19 13:03:57 -0500

    Reduced casting and simplified code in WrapperStructsExtensions.cpp

--- Extra/WrapperStructsExtensions.cpp
+++ Extra/WrapperStructsExtensions.cpp
@@ -4,86 +4,77 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
+constexpr double M_PI_D = 3.14159265358979323846;
+constexpr float  M_PI_F = 3.14159265358979323846f;
+
 Rotator RT::QuatToRotator(Quat quat)
 {
 	//From Arator's HelperFunctions
 	RT::Matrix3 matrix(quat);
 	Vector fwd = matrix.forward;
-	//Vector up = matrix.right;
-	//Vector right = matrix.up;
     Vector right = matrix.right;
     Vector up = matrix.up;
 
 	//Pitch
-	float pitch_f = static_cast<float>(asin(fwd.Z));
-	int PITCH = static_cast<int>((pitch_f / (M_PI / 2)) * 16384);
+	float pitch_f = asinf(fwd.Z);
+	int PITCH = static_cast<int>((pitch_f / (M_PI_F / 2.f)) * 16384.f);
 
 	//Yaw
-	float hor_mag = static_cast<float>(sqrt((double)fwd.X * fwd.X + (double)fwd.Y * fwd.Y));
-	float hor_y = fwd.Y / hor_mag;
-	float fwd_y = static_cast<float>(asin(hor_y));
+	float hor_mag = sqrtf(fwd.X * fwd.X + fwd.Y * fwd.Y);
+	float fwd_y = asinf(fwd.Y / hor_mag);
 	if (fwd_y >= 0)
 	{
-		if (fwd.X >= 0)
-			fwd_y = fwd_y;
-		else
-			fwd_y = static_cast<float>(M_PI) - fwd_y;
+		if (fwd.X < 0)
+			fwd_y = M_PI_F - fwd_y;
 	}
 	else
 	{
-		if (fwd.X >= 0)
-			fwd_y = fwd_y;
-		else
-			fwd_y = static_cast<float>(-M_PI) - fwd_y;
+		if (fwd.X < 0)
+			fwd_y = -M_PI_F - fwd_y;
 	}
-	int YAW = static_cast<int>((fwd_y / M_PI) * 32768);
+	int YAW = static_cast<int>((fwd_y / M_PI_F) * 32768.f);
 
 	//Roll
-	Vector vert = Vector(0, 0, 1);
-	if(up.Z < 0)
-		vert = Vector(0, 0, -1);
-	Vector hor_right = Vector::cross(fwd, vert);
-	hor_right = { -hor_right.X, -hor_right.Y, -hor_right.Z }; // left-handed coordinate system
+    Vector vert = (up.Z < 0) ? Vector(0, 0, -1) : Vector(0, 0, 1);
+	Vector hor_right = Vector::cross(fwd, vert) * -1;// left-handed coordinate system
 	hor_right.normalize();
+
 	float roll_cos = Vector::dot(hor_right, right);
-	float roll_f = static_cast<float>(acos(roll_cos));
-	
-	float up_f = static_cast<float>(asin(up.Z));
+	float roll_f = acosf(roll_cos);
+	float up_f = asinf(up.Z);
 	
 	if (right.Z >= 0)
 	{
 		if (up.Z >= 0)
 			roll_f = -roll_f;
 		else
-			roll_f  = static_cast<float>(-M_PI) + roll_f;
+			roll_f  = -M_PI_F + roll_f;
 	}
 	else
 	{
-		if (up.Z >= 0)
-			roll_f = roll_f;
-		else
-			roll_f = static_cast<float>(M_PI) - roll_f;
+		if (up.Z < 0)
+			roll_f = M_PI_F - roll_f;
 	}
-	int ROLL = static_cast<int>((roll_f / M_PI) * 32768);
+	int ROLL = static_cast<int>((roll_f / M_PI_F) * 32768.f);
 
 	return Rotator(PITCH, YAW, ROLL);
 }
 
 Quat RT::RotatorToQuat(Rotator rot)
 {
-	float rotatorToRadian = static_cast<float>(((M_PI / 180) / 2) / 182.044449);
-	float sinPitch = static_cast<float>(sin((double)rot.Pitch*rotatorToRadian));
-	float cosPitch = static_cast<float>(cos((double)rot.Pitch*rotatorToRadian));
-	float sinYaw = static_cast<float>(sin((double)rot.Yaw*rotatorToRadian));
-	float cosYaw = static_cast<float>(cos((double)rot.Yaw*rotatorToRadian));
-    float sinRoll = static_cast<float>(sin((double)rot.Roll*rotatorToRadian));
-    float cosRoll = static_cast<float>(cos((double)rot.Roll*rotatorToRadian));
+	float rotatorToRadian = (((M_PI_F / 180.f) * .5f) / 182.044449f);
+	float sinPitch = sinf(rot.Pitch * rotatorToRadian);
+	float cosPitch = cosf(rot.Pitch * rotatorToRadian);
+	float sinYaw   = sinf(rot.Yaw   * rotatorToRadian);
+	float cosYaw   = cosf(rot.Yaw   * rotatorToRadian);
+    float sinRoll  = sinf(rot.Roll  * rotatorToRadian);
+    float cosRoll  = cosf(rot.Roll  * rotatorToRadian);
 	
 	Quat convertedQuat;
-	convertedQuat.X = (cosRoll*sinPitch*sinYaw) - (sinRoll*cosPitch*cosYaw);
-	convertedQuat.Y = ((-cosRoll)*sinPitch*cosYaw) - (sinRoll*cosPitch*sinYaw);
-	convertedQuat.Z = (cosRoll*cosPitch*sinYaw) - (sinRoll*sinPitch*cosYaw);
-	convertedQuat.W = (cosRoll*cosPitch*cosYaw) + (sinRoll*sinPitch*sinYaw);
+	convertedQuat.X = (cosRoll  * sinPitch * sinYaw) - (sinRoll * cosPitch * cosYaw);
+	convertedQuat.Y = (-cosRoll * sinPitch * cosYaw) - (sinRoll * cosPitch * sinYaw);
+	convertedQuat.Z = (cosRoll  * cosPitch * sinYaw) - (sinRoll * sinPitch * cosYaw);
+	convertedQuat.W = (cosRoll  * cosPitch * cosYaw) + (sinRoll * sinPitch * sinYaw);
 
 	return convertedQuat;
 }
@@ -92,33 +83,30 @@ Quat RT::Slerp(Quat q1, Quat q2, float percent)
 {
 	//percent range: 0-1
 	//https://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/slerp/index.htm
-	Quat q;
 	float dot = q1.W*q2.W + q1.X*q2.X + q1.Y*q2.Y + q1.Z*q2.Z;
 	// if qa=qb or qa=-qb then theta = 0 and we can return qa
-	if (abs(dot) >= 1.0)
+	if (abs(dot) >= 1.f)
 	{
-		q.W = q1.W;
-		q.X = q1.X;
-		q.Y = q1.Y;
-		q.Z = q1.Z;
-		return q;
+		return q1;
 	}
 	// Calculate temporary values.
-	double halfTheta = acos(dot);
-	double sinHalfTheta = sqrt(1.0 - (double)dot*dot);
+	float halfTheta = acosf(dot);
+	float sinHalfTheta = sqrtf(1.f - dot*dot);
 	// if theta = 180 degrees then result is not fully defined
 	// we could rotate around any axis normal to qa or qb
-	if (fabs(sinHalfTheta) < 0.001)
+	if (fabsf(sinHalfTheta) < 0.001f)
 	{
+        Quat q;
         q.W = q1.W * 0.5f + q2.W * 0.5f;
         q.X = q1.X * 0.5f + q2.X * 0.5f;
         q.Y = q1.Y * 0.5f + q2.Y * 0.5f;
         q.Z = q1.Z * 0.5f + q2.Z * 0.5f;
 		return q;
 	}
-	float ratioA = static_cast<float>(sin((1.0 - percent) * halfTheta) / sinHalfTheta);
-	float ratioB = static_cast<float>(sin(percent * halfTheta) / sinHalfTheta); 
+	float ratioA = sinf((1.f - percent) * halfTheta) / sinHalfTheta;
+	float ratioB = sinf(percent * halfTheta) / sinHalfTheta;
 
+    Quat q;
 	q.W = (q1.W * ratioA + q2.W * ratioB);
 	q.X = (q1.X * ratioA + q2.X * ratioB);
 	q.Y = (q1.Y * ratioA + q2.Y * ratioB);
@@ -129,7 +117,7 @@ Quat RT::Slerp(Quat q1, Quat q2, float percent)
 
 Quat RT::NormalizeQuat(Quat q)
 {
-	float mag = static_cast<float>(sqrt((double)q.X*q.X + (double)q.Y*q.Y + (double)q.Z*q.Z + (double)q.W*q.W));
+	float mag = sqrtf(q.X*q.X + q.Y*q.Y + q.Z*q.Z + q.W*q.W);
 	q.X /= mag;
 	q.Y /= mag;
 	q.Z /= mag;
